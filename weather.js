0121c257edd3f32a5de677c1a1aac65f3f339751
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-17 13:36:03 +0300

    try commit

--- weather.js
+++ weather.js
@@ -14,13 +14,24 @@ form.addEventListener("submit",(e)=>{
     getWeatherDataFromApi();
 })
 
-const getWeatherDataFromApi = () => {
+const getWeatherDataFromApi = async () => {
     // alert("http requst gone");
     let apikey = DecryptStringAES(localStorage.getItem("apiKey"));
     let inputVal = input.value;
     let unitType = "metric";
     let lang = "tr";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apikey}&units=${unitType}&lang=${lang}`;
+
+    try {
+        // const response = await fetch(url).then(response => response.json());
+        const response = await axios(url);
+        const {name,main,sys,weather} = response.data;
+        console.log(response.data);
+        let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    } 
+    catch (error) {
+        
+    }
     form.reset();
 
 }
